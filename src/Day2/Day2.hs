2a1a638edbfc8f6d9ca8763e90dc42859b490589
bangn <thebangnguyen@gmail.com>
2019-12-03 01:02:47 +1100

    Solve day 2 part 1

--- src/Day2/Day2.hs
+++ src/Day2/Day2.hs
@@ -1,5 +1,41 @@
 module Day2 (
 ) where
 
+import Data.List.Split
+import System.IO (readFile)
+
+type IntCode = Int
+type Program = [IntCode]
+
 main :: IO ()
-main = putStrLn "Day 2 starts"
+main = do
+  program <- fetchProgram
+  print . head . rebuild 0 $ setAt (setAt program 1 12) 2 2
+
+fetchProgram :: IO Program
+fetchProgram = do
+  contents <- readFile "./src/Day2/input.txt"
+  pure . fmap (read :: String -> IntCode) . splitOn "," $ contents
+
+rebuild :: Int -> Program -> Program
+rebuild currentPos program =
+  if optCode == 99
+    then program
+    else
+      let operand1 = program !! (program !! (currentPos + 1))
+          operand2 = program !! (program !! (currentPos + 2))
+          replacePos = program !! (currentPos + 3)
+          nextPos = currentPos + 4
+        in
+          if optCode == 1
+            then rebuild nextPos $ setAt program replacePos (operand1 + operand2)
+            else if optCode == 2
+                    then rebuild nextPos $ setAt program replacePos (operand1 * operand2)
+                    else undefined
+  where optCode = program !! currentPos
+
+setAt :: [a] -> Int -> a -> [a]
+setAt xs n x =
+  let (xs', _:xs'') = splitAt n xs
+   in xs' ++ [x] ++ xs''
+
