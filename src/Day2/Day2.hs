d8a279c82153ba8eefb0d39bbc7db3c0b862c43d
bangn <thebangnguyen@gmail.com>
2019-12-03 01:40:59 +1100

    Solve day 2 part 2

--- src/Day2/Day2.hs
+++ src/Day2/Day2.hs
@@ -10,29 +10,44 @@ type Program = [IntCode]
 main :: IO ()
 main = do
   program <- fetchProgram
-  print . head . rebuild 0 $ setAt (setAt program 1 12) 2 2
+  let result = rebuild (0, setAt (setAt program 1 12) 2 2)
+  print . head . snd $ result
+  let (noun, verb) = findNounAndVerb program 19690720
+  print $ 100 * noun + verb
+
+findNounAndVerb :: Program -> Int -> (Int, Int)
+findNounAndVerb program expected =
+  head [
+    (noun, verb) |
+      noun <- [0..99],
+      verb <- [0..99],
+        let result = rebuild (0, setAt (setAt program 1 noun) 2 verb)
+          in
+            (head . snd $ result) == expected
+ ]
+
 
 fetchProgram :: IO Program
 fetchProgram = do
   contents <- readFile "./src/Day2/input.txt"
   pure . fmap (read :: String -> IntCode) . splitOn "," $ contents
 
-rebuild :: Int -> Program -> Program
-rebuild currentPos program =
+rebuild :: (Int, Program) -> (Int, Program)
+rebuild (ip, program)=
   if optCode == 99
-    then program
+    then (ip + 1, program)
     else
-      let operand1 = program !! (program !! (currentPos + 1))
-          operand2 = program !! (program !! (currentPos + 2))
-          replacePos = program !! (currentPos + 3)
-          nextPos = currentPos + 4
+      let operand1 = program !! (program !! (ip + 1))
+          operand2 = program !! (program !! (ip + 2))
+          replacePos = program !! (ip + 3)
+          nextPos = ip + 4
         in
           if optCode == 1
-            then rebuild nextPos $ setAt program replacePos (operand1 + operand2)
+            then rebuild (nextPos, setAt program replacePos $ operand1 + operand2)
             else if optCode == 2
-                    then rebuild nextPos $ setAt program replacePos (operand1 * operand2)
+                    then rebuild (nextPos, setAt program replacePos $ operand1 * operand2)
                     else undefined
-  where optCode = program !! currentPos
+  where optCode = program !! ip
 
 setAt :: [a] -> Int -> a -> [a]
 setAt xs n x =
